362aad687aa066bbb621ee053b41e2b4f63ff0d1
Xavier Brinon <xavier.brinon@ecs.co.uk>
2021-01-13 16:21:07 +0000

    remove template extras

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
 import React from 'react'
 import './App.css';
 
 function App () {
-  const ccpContainerRef = React.useRef(window.document.getElementById('root'))
+  const ccpContainerRef = React.useRef(window.document.getElementById('ccpContainer'))
 
   React.useEffect(() => {
     console.log({ connect: window.connect })
     window.connect?.core?.initCCP(ccpContainerRef.current, {
-      ccpUrl: 'https://dlg-connect-dev.awsapps.com/connect/ccp-v2/', 
-      // ccpUrl: 'https://cx-ecs-chandank.awsapps.com/connect/ccp-v2/',
+      ccpUrl: 'https://dlg-connect-dev.awsapps.com/connect/ccp-v2/',
       loginPopup: true,
-      // loginUrl: REACT_APP_LOGIN_URL,
       softphone: {
         allowFramedSoftphone: true,
         disabledRingtone: false,
@@ -21,19 +18,8 @@ function App () {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" id="ccpContainer">
+        <p>test</p>
       </header>
     </div>
   );
