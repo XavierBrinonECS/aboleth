e81b89a7d3652c74ec45a7b6c9b56014517e7eea
Xavier Brinon <xavier.brinon@ecs.co.uk>
2021-01-15 20:58:49 +0000

    add connection to onclick event

--- src/App.js
+++ src/App.js
@@ -79,17 +79,27 @@ function App () {
 
   function ascend () {
     var agent = new window.connect.Agent();
-    // var queueARN = "arn:aws:connect:eu-west-2:172096265603:instance/fe234290-4bc8-492f-a601-3c4b29259ae2/transfer-destination/f893010f-7d84-42a2-afb8-d8287050ad57";
-
-    agent.connect(secureIVREndpoint, {
-      // queueARN,
-      success: function () { console.log("outbound call connected"); },
-      failure: function (err) {
-        console.log("outbound call connection failed");
-        console.log(err);
-      }
-    });
+    let secIVR = {}
+    agent.getEndpoints(
+      agent.getAllQueueARNs(), {
+      success: ({
+        endpoints
+      }) => {
+        console.log({ endpoints })
+        secIVR = endpoints
+          .filter(endpoint => /SecureIVR/i.test(endpoint.name))
+          .pop()
+      },
+      failure: data => console.log({ failure: data })
+    }
+    )
+    agent
+      .getContacts(window.connect.ContactType.VOICE)
+      .pop()
+      ?.addConnection(secIVR, {
+        success: data => { console.log({ success: data }) },
+        failure: data => { console.log({ failure: data }) }
+      })
   }
 }
-
 export default App;
